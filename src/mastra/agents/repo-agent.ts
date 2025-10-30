d2bdb19315077cc6014aaf67ce6c1b30f1351c9e
python-fuse <hauwamuktar880@gmail.com>
2025-10-30 16:12:57 +0100

    feat: created the repoagent

--- src/mastra/agents/repo-agent.ts
+++ src/mastra/agents/repo-agent.ts
@@ -1,5 +1,55 @@
 import { Agent } from "@mastra/core/agent";
 import { Memory } from "@mastra/memory";
 import { LibSQLStore } from "@mastra/libsql";
-import { repoTool } from "../tools/repo-tool";
-import { scorers } from "../scorers/repo-scorer";
+import { repoTool } from "../tools/repo-tools";
+// import { scorers } from "../scorers/repo-scorer";
+
+const DEFAULT_REPO_AGENT_NAME = "Repo Pulse";
+const DEFAULT_REPO_AGENT_DESCRIPTION =
+  "An agent designed to analyze the activity and health of a code repository.";
+
+export const RepoAgent = new Agent({
+  name: DEFAULT_REPO_AGENT_NAME,
+  instructions: `You are a helpful code repository assistant that provides insights and analysis on code repositories.
+        Your primary function is to help users understand the activity,health and trends of a code repository. 
+        When responding: 
+        - Always ask for a repository URL if none is provided
+        - If the repository URL isn't valid, please inform the user
+        - if only a name is given, try to infer the URL or ask for clarification
+        - Keep responses concise but informative
+        - Use the repoTool to fetch repository data.
+        - If the user asks for specific metrics, provide them based on the latest data available.
+        - If the user asks for trends, analyze the data and provide insights based on historical activity.
+        - If the user asks if he can contribute to the repository, provide guidelines based on the repository's contribution policies.
+
+        Use the repoTool to fetch repository data.
+    `,
+  model: "google/gemini-2.5-pro",
+  tools: { repoTool },
+  // scorers: {
+  //     toolCallAppropriateness: {
+  //         scorer: scorers.toolCallAppropriatenessScorer,
+  //         sampling: {
+  //             type: "ratio",
+  //             rate: 1,
+  //         },
+  //     },
+  //     completeness: {
+  //         scorer: scorers.completenessScorer,
+  //         sampling: {
+  //             type: "ratio",
+  //             rate: 1,
+  //         },
+  //     },
+  //     translation: {
+  //         scorer: scorers.translationScorer,
+  //         sampling: {
+  //             type: "ratio",
+  //             rate: 1,
+  //         },
+  memory: new Memory({
+    storage: new LibSQLStore({
+      url: "file:../mastra.db",
+    }),
+  }),
+});
