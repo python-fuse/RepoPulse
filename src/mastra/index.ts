c10a23ad955dfaa2e9fa63cadc0c728541fbcf13
python-fuse <hauwamuktar880@gmail.com>
2025-10-31 11:39:06 +0100

    fix: fixed faulty custom route

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,6 +2,7 @@ import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
 import { LibSQLStore } from "@mastra/libsql";
 import { RepoAgent } from "./agents/repo-agent";
+import { a2aAgentApiRoute } from "./routes/a2aRoute";
 
 export const mastra = new Mastra({
   agents: { repoAgent: RepoAgent },
@@ -14,7 +15,7 @@ export const mastra = new Mastra({
     level: "info",
   }),
   server: {
-    // apiRoutes:[a2aAgentApiRoute]
+    apiRoutes: [a2aAgentApiRoute],
   },
   telemetry: {
     // Telemetry is deprecated and will be removed in the Nov 4th release
